02a40a60c23618deec15cec066e55b76fb27349b
Vishal Kumar Kushwaha <123886831+Kushwaha2219@users.noreply.github.com>
2024-08-28 23:33:12 +0530

    Pattern4.java

---
+++ Pattern4.java
@@ -0,0 +1,24 @@
+public class Solution {
+    public static void nTriangle(int n) {
+        // Write your code here
+        int m = 1;
+        for(int i = 0 ;i < n ; i++){
+            for(int j = 0 ; j<= i ;j++){
+                System.out.print(m +" ");
+            }
+            m++;
+            System.out.println();
+        }
+    }
+}
+
+/*
+
+Input: ‘N’ = 3
+
+Output: 
+1
+2 2 
+3 3 3
+
+  */
